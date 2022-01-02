4f027a28c4d0f73de8ee3386124d751e489b4c12
heberpedro <heberv.pedro@gmail.com>
2022-01-02 11:59:51 -0300

    chore: removing unnecessary libs and files

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,6 +1,5 @@
+import { BASE_URL } from '@/constants'
 import axios from 'axios'
-
-import { BASE_URL } from '../constants'
 
 const api = axios.create({
   baseURL: BASE_URL,
--- src/store/modules/rootReducer.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-
-import { StoreState } from '../createStore'
-import auth from './auth/reducer'
-
-export default combineReducers<StoreState>({
-  auth,
-  _persist: (state: any = null) => state,
-})
