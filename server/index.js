f415c55be166affb2ffcbb317b24b154010519fb
heberpedro <heberv.pedro@gmail.com>
2022-01-02 12:01:37 -0300

    chore: mock server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const jsonServer = require('json-server')
+
+const data = require('./data.json')
+
+const middlewares = jsonServer.defaults()
+const routes = jsonServer.router(data)
+const app = jsonServer.create()
+
+app.use(middlewares)
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  next()
+})
+app.use(routes)
+
+app.listen(3333, () =>
+  console.log('🔥 Server started at http://localhost:3333')
+)
