a1605cbf778cd451d525b693417fd3e8f9f95703
heberpedro <heberv.pedro@gmail.com>
2022-01-14 17:48:50 -0300

    refactor: list contacts and message error

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,48 +1,42 @@
-import { useState, useEffect, ChangeEvent } from 'react'
+import { useState, useEffect, ChangeEvent, useCallback } from 'react'
 import { Link } from 'react-router-dom'
 
 import arrow from '@/assets/images/icons/arrow.svg'
 import edit from '@/assets/images/icons/edit.svg'
 import trash from '@/assets/images/icons/trash.svg'
-import api from '@/services/api'
+import sad from '@/assets/images/sad.svg'
+import { Contact } from '@/common/types/api'
+import { fetchContacts } from '@/services/api/contacts'
 
+import Button from '@/components/Button'
 import Loader from '@/components/Loader'
 
 import * as S from './styles'
-
-interface Contact {
-  id: number
-  name: string
-  email?: string
-  phone?: string
-  category?: string
-}
 
 const ContactsList = () => {
   const [contacts, setContacts] = useState<Contact[]>([])
   const [orderBy, setOrderBy] = useState('asc')
   const [searchTerm, setSearchTerm] = useState('')
   const [isLoading, setIsLoading] = useState(true)
+  const [hasError, setHasError] = useState(false)
+
+  const loadContacts = useCallback(async () => {
+    try {
+      setIsLoading(true)
+      const contactsList = await fetchContacts({ searchTerm, orderBy })
+
+      setHasError(false)
+      setContacts(contactsList)
+    } catch (error) {
+      setHasError(true)
+    } finally {
+      setIsLoading(false)
+    }
+  }, [orderBy, searchTerm])
 
   useEffect(() => {
-    async function loadContacts() {
-      try {
-        setIsLoading(true)
-
-        const { data } = await api.get<Contact[]>(
-          `/contacts?&name_like=${searchTerm}&_sort=name&_order=${orderBy}`
-        )
-
-        setContacts(data)
-      } catch (error) {
-        console.log(error)
-      } finally {
-        setIsLoading(false)
-      }
-    }
-
     loadContacts()
-  }, [orderBy, searchTerm])
+  }, [loadContacts])
 
   const handleToggleOrderBy = () => {
     setOrderBy((prevState) => (prevState === 'asc' ? 'desc' : 'asc'))
@@ -51,6 +45,8 @@ const ContactsList = () => {
   const handleChangeSearchTerm = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value)
   }
+
+  const handleTryAgain = () => loadContacts()
 
   return (
     <S.Container>
@@ -65,45 +61,66 @@ const ContactsList = () => {
         />
       </S.InputSearchContainer>
 
-      <S.Header>
-        <strong>
-          {contacts.length} {contacts.length === 1 ? ' contato' : 'contatos'}
-        </strong>
+      <S.Header hasError={hasError}>
+        {!hasError && (
+          <strong>
+            {contacts.length} {contacts.length === 1 ? ' contato' : 'contatos'}
+          </strong>
+        )}
+
         <Link to="/new">Novo Contato</Link>
       </S.Header>
 
-      {contacts.length > 0 && (
-        <S.ListHeader orderBy={orderBy}>
-          <header>
-            <button type="button" onClick={handleToggleOrderBy}>
-              <span>Nome</span>
-              <img src={arrow} alt="Arrow" />
-            </button>
-          </header>
-        </S.ListHeader>
+      {hasError && (
+        <S.ErrorContainer>
+          <img src={sad} alt="Sad" />
+
+          <div className="details">
+            <strong>Ocorreu um erro ao obter os seus contatos!</strong>
+
+            <Button type="button" onClick={handleTryAgain}>
+              Tentar novamente
+            </Button>
+          </div>
+        </S.ErrorContainer>
       )}
 
-      {contacts.map((contact) => (
-        <S.Card key={contact.id}>
-          <div className="info">
-            <div className="contact-name">
-              <strong>{contact.name}</strong>
-              {contact.category && <small>{contact.category}</small>}
-            </div>
-            <span>{contact.email}</span>
-            <span>{contact.phone}</span>
-          </div>
+      {!hasError && (
+        <>
+          {contacts.length > 0 && (
+            <S.ListHeader orderBy={orderBy}>
+              <header>
+                <button type="button" onClick={handleToggleOrderBy}>
+                  <span>Nome</span>
+                  <img src={arrow} alt="Arrow" />
+                </button>
+              </header>
+            </S.ListHeader>
+          )}
 
-          <div className="actions">
-            <Link to={`/edit/${contact.id}`}>
-              <img src={edit} alt="Edit" />
-            </Link>
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
-          </div>
-        </S.Card>
-      ))}
+          {contacts.map((contact) => (
+            <S.Card key={contact.id}>
+              <div className="info">
+                <div className="contact-name">
+                  <strong>{contact.name}</strong>
+                  {contact.category && <small>{contact.category}</small>}
+                </div>
+                <span>{contact.email}</span>
+                <span>{contact.phone}</span>
+              </div>
+
+              <div className="actions">
+                <Link to={`/edit/${contact.id}`}>
+                  <img src={edit} alt="Edit" />
+                </Link>
+                <button type="button">
+                  <img src={trash} alt="Delete" />
+                </button>
+              </div>
+            </S.Card>
+          ))}
+        </>
+      )}
     </S.Container>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 
+interface HeaderProps {
+  hasError: boolean
+}
+
 interface ListHeaderProps {
   orderBy: string
 }
@@ -27,11 +31,14 @@ export const InputSearchContainer = styled.div`
   }
 `
 
-export const Header = styled.header`
+export const Header = styled.header<HeaderProps>`
   margin-top: 32px;
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: ${({ hasError }) =>
+    hasError ? 'flex-end' : 'space-between'};
+  border-bottom: 2px solid ${({ theme }) => theme.colors.gray[100]};
+  padding-bottom: 16px;
 
   strong {
     font-size: 24px;
@@ -125,6 +132,23 @@ export const Card = styled.div`
       background: transparent;
       border: none;
       margin-left: 8px;
+    }
+  }
+`
+
+export const ErrorContainer = styled.div`
+  margin-top: 16px;
+  display: flex;
+  align-items: center;
+
+  .details {
+    margin-left: 24px;
+
+    strong {
+      font-size: 22px;
+      color: ${({ theme }) => theme.colors.danger.main};
+      display: block;
+      margin-bottom: 8px;
     }
   }
 `
